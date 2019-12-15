65da7f433ab8b58dbbb0a73818a457a71fa6817c
ctran30 <54650720+ctran30@users.noreply.github.com>
2019-12-15 15:37:42 -0800

    Create todo.h

---
+++ todo.h
@@ -0,0 +1,34 @@
+#ifndef TODO_H_
+#define TODO_H_
+#include <string>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+struct myTask{
+	string taskName;
+	string dueDate;
+	int priority;
+};
+
+class ToDoList{
+public:
+	ToDoList();
+	//ToDoList(string name, string date, int p);
+	bool addToList(struct myTask newTask);
+	bool addToList(string name, string date, int p);
+	bool getNextTask(struct myTask &task);
+	bool getNextTask(string &name, string &date, int &p);
+	void printList();
+
+private:
+	ToDoList *list;
+	int tasks;
+	int nextTask;
+	int maxSize;
+};
+
+
+
+#endif /* TODO_H_ */
+
