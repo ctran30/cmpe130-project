8c7327bcde7f669c52dcb141d6f743da375aafd7
Sarfaraz-Mohammed <54648151+Sarfaraz-Mohammed@users.noreply.github.com>
2019-12-15 18:13:23 -0800

    Update QuickTaskapp.cpp

--- QuickTaskapp.cpp
+++ QuickTaskapp.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include "sort.h"
 #include "todo.h"
@@ -6,51 +7,57 @@ using namespace std;
 
 int main() {
 
-	char exit = 'n';
-	int choice = 0;
+	ToDoList list;
 
-	while(exit = 'n')
+	int choice = 0;
+	char answer;
+
+	while(choice <= 4)
 	{
 		cout << "Welcome to QuickTask: Easy To-Do List!" <<endl;
 		cout <<"Choose an option: " <<endl;
 		cout <<"1. Create new task" <<endl;
 		cout <<"2. Show next task in my list" <<endl;
 		cout <<"3. Search for a task" <<endl;
-		cout <<"4. Modify a task" <<endl;
-		cout <<"5. Remove a task" <<endl;
-		cout <<"6. Print out all my tasks" <<endl;
+		cout <<"4. Print out all my tasks" <<endl;
 
 		cin >> choice;
 
 		if(choice == 1)
 		{
-			string name, description;
-			int month, day, year;
+			string name, date;
+			int priority;
 
 			cout <<"Enter your task name: " <<endl;
 			cin >> name;
-			cout <<"Enter the month of completion(ex. 5 for May): " <<endl;
-			cin >> month;
-			cout <<"Enter the day of completion(ex. 15): " <<endl;
-			cin >> day;
-			cout <<"Enter the year of completion(ex. 2019): " <<endl;
-			cin >> year;
-			cout << "Enter a short description or details: " <<endl;
-			cin >> description;
+			cout <<"Enter the date of completion(mm/dd): " <<endl;
+			cin >> date;
+			cout << "Enter the priority of this task from 1-5(1 being not important and 5 being very important: " <<endl;
+			cin >> priority;
 
+			list.addToList(name, date, priority);
 
+		}else if(choice == 2)
+		{
 
+			ToDoList getNextTask();
+
+		}else if(choice == 3)
+		{
+
+			ToDoList search();
+
+		}else if(choice == 4)
+		{
+			list.printList();
+		}else
+		{
+			cout << "Invalid Option!" << endl;
 		}
+		cout << "Would you like to exit? y/n" <<endl;
+		cin >> answer;
 
 
 	}
-
-
-
-
-
-
-
 	return 0;
 }
-
