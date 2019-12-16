2f98d06f05471cfdfd569ae1d599c3c07b365d67
Sarfaraz-Mohammed <54648151+Sarfaraz-Mohammed@users.noreply.github.com>
2019-12-15 18:14:37 -0800

    Update todo.h

--- todo.h
+++ todo.h
@@ -5,30 +5,26 @@
 #include <iomanip>
 using namespace std;
 
-struct myTask{
-	string taskName;
-	string dueDate;
-	int priority;
-};
+
 
 class ToDoList{
 public:
 	ToDoList();
-	//ToDoList(string name, string date, int p);
-	bool addToList(struct myTask newTask);
 	bool addToList(string name, string date, int p);
-	bool getNextTask(struct myTask &task);
-	bool getNextTask(string &name, string &date, int &p);
+	void getNextTask(ToDoList *list);
 	void printList();
+	void print(string name, string date, int p);
 
 private:
 	ToDoList *list;
 	int tasks;
 	int nextTask;
 	int maxSize;
+	string taskName;
+	string dueDate;
+	int priority;
 };
 
 
 
 #endif /* TODO_H_ */
-
